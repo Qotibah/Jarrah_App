968f3efbaf7a0614b6dffbe453b3d7be985f6832
Elias Al-Shwiky <eliasessa901@gmail.com>
2025-07-07 00:40:45 +0300

    s

--- App.tsx
+++ App.tsx
@@ -5,7 +5,9 @@ import RoleSelectionScreen from './src/screens/RoleSelectionScreen';
 import BuyerLoginScreen from './src/buyer/BuyerLoginScreen';
 import BuyerOrderScreen from './src/buyer/BuyerOrderScreen';
 import { RootStackParamList } from './src/types/navigation';
-import MapPickerScreen from './src/buyer/MapPickerScreen'; // تأكد من وجود هذا المسار
+
+import MapPickerScreen from './src/buyer/MapPickerScreen'; 
+import SellerLoginScreen from './src/sallar/SellerLoginScreen'; // تأكد من وجود هذا المسار
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 // مؤقتًا سنستخدم شاشات فاضية لمكان تسجيل الدخول
@@ -19,6 +21,7 @@ export default function App() {
         <Stack.Screen name="BuyerLogin" component={BuyerLoginScreen} />
         <Stack.Screen name="BuyerOrder" component={BuyerOrderScreen} />
          <Stack.Screen name="MapPicker" component={MapPickerScreen} />
+        <Stack.Screen name="SellerLogin" component={SellerLoginScreen} />
 
       </Stack.Navigator>
     </NavigationContainer>
